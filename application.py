8f9c51f7ee4cc6e41f3c47c1662cc72072027e5d
Mykola Lev <udent@protonmail.com>
2020-04-10 13:30:35 +0300

    Revert "change name"
    This reverts commit 03c90c3fa1fd97c2860be0fc450f2cfa907eb666.

--- application.py
+++ application.py
@@ -6,25 +6,25 @@
 # ----------------------------------------------
 from flask import Flask, render_template
 
-app = Flask(__name__,
-            static_url_path='', 
-            static_folder='static/')
+application = Flask(__name__,
+					static_url_path='', 
+            		static_folder='static/',)
 
 
-@app.route("/")
+@application.route("/")
 def root():
     return render_template("index.html")
 
-@app.route("/about")
+@application.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/version")
+@application.route("/version")
 def app_version():
     return "2.0"
 
 #--------Main------------------
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    application.debug = True
+    application.run()
 #------------------------------
