668f76189b4fdba68165ef5dc6371c41d36005cf
Mykola Lev <udent@protonmail.com>
2020-04-11 12:34:17 +0300

    added an ajax js function that checks who is playing and shows it in the left corner of the page. and a lobby music when nothing is on air.

--- application.py
+++ application.py
@@ -5,15 +5,37 @@
 #
 # ----------------------------------------------
 from flask import Flask, render_template
+import json
 
 application = Flask(__name__,
-					static_url_path='', 
-            		static_folder='static/',)
+                    static_url_path='', 
+                    static_folder='static/',)
 
 
 @application.route("/")
 def root():
     return render_template("index.html")
+
+def get_data_from_radio_server():
+    # request page form radio
+    url = "radio.tusovka.ml/status-json.xsl"
+    # parse dj and artist and title
+    # return in dictionary style
+    return json.loads(parsed_data)
+
+@application.route("/whats_playin")
+def whats_playin():
+    who_and_what = {'dj': None,'artist': None,'title': None}
+    data_from_radio_server = get_data_from_radio_server()
+
+    if data_from_radio_server:
+        who_and_what['dj'] = data_from_radio_server['dj']
+        who_and_what['artist'] = data_from_radio_server['artist']
+        who_and_what['title'] = data_from_radio_server['title']
+    else:
+        who_and_what['dj'] = 'nobody'
+
+    return json.dumps(who_and_what)
 
 @application.route("/about")
 def about():
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,8 @@
+whats_cookin();
+
 function play(id){
+  clearTimeout(whats_cookin);
+  whats_cookin();
   player = document.getElementById(id);
   bg = document.getElementById('video-player');
   player.load();
@@ -7,13 +11,14 @@ function play(id){
 }
 
 function pause(id){
+  whats_cookin("stop");
   player = document.getElementById(id);
   bg = document.getElementById('video-player');
   player.pause();
   bg.pause();
 }
 
-volume_click_counter=0
+volume_click_counter = 0;
 
 function volume(id, direction){
   player = document.getElementById(id);
@@ -38,5 +43,46 @@ function volume(id, direction){
   	} else {
   		player.volume = player.volume + 1/steps;
   	}
+  }
+}
+
+function whats_cookin(mode) {
+  if (mode == "stop") {
+    clearTimeout(whats_cookin);
+    document.getElementById("whos_playin").innerHTML = "";
+    document.getElementById("whats_playin").innerHTML = "Paused";
+    return;
+  }
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange=function() {
+    if (this.readyState == 4 && this.status == 200) {
+      parse_who_and_what(this.responseText);
+    }
+  };
+  xhttp.open("GET", "radio.tusovka.ml/status-json.xsl", true);
+  xhttp.send();
+
+  // checking who is playing every 30 seconds
+  setTimeout(whats_cookin, 30000);
+}
+
+function parse_who_and_what(raw_json_data) {
+  var json_data = JSON.parse(raw_json_data);
+  if ( json_data.icestats.source ) {
+    document.getElementById("whos_playin").innerHTML = json_data.icestats.source[0].server_name;
+    document.getElementById("whos_playin").innerHTML.href = '/'+json_data.icestats.source[0].server_name;
+
+    // mp3 and ogg have different format of metadata in resulting json, need to parse them differently
+
+    if ( json_data.icestats.source[0].server_type == "audio/mpeg" ){
+      document.getElementById("whats_playin").innerHTML = json_data.icestats.source[0].title;
+    } else if ( json_data.icestats.source[0].server_type == "application/ogg" ){
+      document.getElementById("whats_playin").innerHTML = json_data.icestats.source[0].artist + json_data.icestats.source[0].title;
+    } else {
+      document.getElementById("whats_playin").innerHTML = "ID : ID";
+    }
+  } else {
+    document.getElementById("whos_playin").innerHTML = "Nobody's playing..";
+    document.getElementById("whats_playin").innerHTML = "Check back later ;)";
   }
 }
