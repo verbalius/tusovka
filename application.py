0ce8c4492860de9f0c5726ffb960be86552dea49
Mykola Lev <mykola.lev@gmail.com>
2020-09-11 13:59:06 +0300

    radio

--- application.py
+++ application.py
@@ -36,7 +36,7 @@ def root():
 
 @application.route("/whats_playin")
 def whats_playin():
-    return get('localhost:8000/status-json.xsl').content
+    return get('http://radio.tusovka.ml:8000/status-json.xsl').content
 
 
 @application.route("/whos_here")
